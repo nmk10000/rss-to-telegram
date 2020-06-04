127919bbe265e2e3e707e5e1a3fbcce8ad248b8f
nmk10000 <58908219+nmk10000@users.noreply.github.com>
2020-06-04 23:44:32 +0530

    Update and rename config.example.py to config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+"""
+This is a sample config file
+Copy this to config.py and fill in your actual data before starting
+"""
+
+config = {
+    "telegram-token": "1296488130:AAH_Qi3WpsvlHy8Q9A-CEqib1TLjSxIw0kI",
+    "channel-id": "-1001114081400",
+    "feeds": ["https://malayalam.oneindia.com/rss/malayalam-kozhikode-fb.xml", "https://malayalam.oneindia.com/rss/malayalam-malappuram-fb.xml"]
+}
